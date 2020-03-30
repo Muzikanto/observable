3b849ad81610808f98232ec2569eaee123f5e8e1
Maxim <mshiriy@legendcity.app>
2020-03-30 12:46:45 +0300

    fix Effect

--- src/Effect.ts
+++ src/Effect.ts
@@ -1,6 +1,7 @@
 import createEvent from "./createEvent";
+import {IEvent} from './Event';
 
-export type IEffect<Req, Res, Err = Error> = (request: Req) => Promise<Res>;
+export type IEffect<Req, Res, Err = Error> = ((request: Req) => Promise<Res>) & { done: IEvent<Res>; fail: IEvent<Err>; loading: IEvent<boolean> };
 
 class Effect<Req, Res, Err = Error> {
     public done = createEvent<Res>();
@@ -10,25 +11,31 @@ class Effect<Req, Res, Err = Error> {
 
     constructor(handler: (params: Req) => Promise<Res>) {
         this.handler = handler;
+
+        // @ts-ignore
+        this.call = (request: Req) => {
+            this.loading(true);
+
+            return new Promise(((resolve: (response: Res) => void, reject: (err: Err) => void) => {
+                this.handler(request)
+                    .then((response) => {
+                        this.done(response);
+                        this.loading(false);
+                        resolve(response);
+                    })
+                    .catch((err) => {
+                        this.fail(err);
+                        this.loading(false);
+                        reject(err);
+                    })
+            }));
+        };
+        this.call.done = this.done;
+        this.call.fail = this.fail;
+        this.call.loading = this.loading;
     }
 
-    public call(request: Req) {
-        this.loading(true);
-
-        return new Promise(((resolve: (response: Res) => void, reject: (err: Err) => void) => {
-            this.handler(request)
-                .then((response) => {
-                    this.done(response);
-                    this.loading(false);
-                    resolve(response);
-                })
-                .catch((err) => {
-                    this.fail(err);
-                    this.loading(false);
-                    reject(err);
-                })
-        }));
-    }
+    public call: IEffect<Req, Res, Err>;
 }
 
 export default Effect;
