0f3ff94a3453af92fa064fa44af51a90e2fcde63
muzikanto <v89911112@mail.ru>
2020-04-04 17:09:03 +0300

    fixup! renew combine args & renew readme

--- test/combine.test.ts
+++ test/combine.test.ts
@@ -6,12 +6,12 @@ describe('combine', () => {
         const num = createStore(1);
         const obj = createStore({test: '-two'});
 
-        const combineStore = combine({
-            map: {num, obj},
-            func: ({num, obj}) => {
+        const combineStore = combine(
+            {num, obj},
+            ({num, obj}) => {
                 return num + obj.test;
             },
-        });
+        );
 
         expect(combineStore.get()).toBe('1-two');
     });
