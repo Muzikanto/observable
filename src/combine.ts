fb191f2d59ed33e54cd34ac10c9503b641192d0c
muzikanto <v89911112@mail.ru>
2020-05-25 00:00:44 +0300

    fix combine

--- src/combine.ts
+++ src/combine.ts
@@ -1,31 +1,38 @@
 import createStore from './createStore';
 import { Store } from './Observable';
 
+export type CombineStore<S> = Store<S> & { _listeners: { [key: string]: () => void } };
+
 function combine<Map extends { [key: string]: any }, S = Map>(
    map: { [k in keyof Map]: Store<Map[k]> },
    func?: (map: Map) => S,
-): Store<S> {
+): CombineStore<S> {
    const state = Object.keys(map).reduce(
       (acc, key) => ({ ...acc, [key]: map[key].get() }),
       {} as Map,
    ) as Map;
+   const listeners: { [key: string]: () => void } = {};
 
-   const store = createStore(func ? func(state) : state);
+   const store = createStore(func ? func(state) : state) as CombineStore<S>;
+   store._listeners = listeners;
 
    const changer = (key: keyof Map, value: Map[typeof key]) => {
-      store.set({
-         ...store.get(),
+      const newState = Object.keys(map).reduce((acc, key) => ({ ...acc, [key]: map[key].get() }), {
          [key]: value,
-      });
+      } as Map) as Map;
+
+      store.set((func ? func(newState) : newState) as S);
    };
 
    for (const key in map) {
-      map[key].watch(nextState => {
+      const unWatch = map[key].watch(nextState => {
          changer(key, nextState);
       });
+
+      listeners[key] = unWatch;
    }
 
-   return store as Store<S>;
+   return store;
 }
 
 export default combine;
