f1b9e790c3037c54254470bcbdb2a2db900950e4
muzikanto <v89911112@mail.ru>
2020-06-28 13:55:25 +0300

    fix effect

--- src/Effect.ts
+++ src/Effect.ts
@@ -57,8 +57,6 @@ class Effect<Req, Res, Err = Error> {
                   .catch((err) => {
                      this.fail(err);
                      this.loading(false);
-
-                     return err;
                   });
             } else {
                this.done(this.cacheData);
